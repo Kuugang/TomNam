6d0787977da695ae3c4691eb0e28b186df889a90
Charlene Repuesto <repuestocharlene@gmail.com>
2024-11-18 23:18:18 +0800

    add initial proof of business model

--- Models/DTO/KarenderyaDTO.cs
+++ Models/DTO/KarenderyaDTO.cs
@@ -2,30 +2,57 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TomNam.Models.DTO
 {
-    public class KarenderyaDTO
-    {
+	public class KarenderyaDTO
+	{
 
 
-        public class EditRequest
-        {
+	  public class EditRequest
+	  {
+		[Required]
+		public required string KarenderyaId { get; set; } // uuid of karenderya
+		[Required]
+		public required string Name { get; set; }
+		[Required]
+		public required string LocationStreet { get; set; }
+		[Required]
+		public required string LocationBarangay { get; set; }
+		[Required]
+		public required string LocationCity { get; set; }
+		[Required]
+		public required string LocationProvince { get; set; }
+		public string? Description { get; set; }
+		public string? LogoPhoto { get; set; }
+		public string? CoverPhoto { get; set; }
+	  }
 
-        }
-
-        public class Create
-        {
-            [Required]
-            public required string Name { get; set; }
-            [Required]
-            public required string LocationStreet { get; set; }
-            [Required]
-            public required string LocationBarangay { get; set; }
-            [Required]
-            public required string LocationCity { get; set; }
-            [Required]
-            public required string LocationProvince { get; set; }
-            public string? Description { get; set; }
-            public string? LogoPhoto { get; set; }
-            public string? CoverPhoto { get; set; }
-        }
-    }
+	  public class Create
+	  {
+		[Required]
+		public required string Name { get; set; }
+		[Required]
+		public required string LocationStreet { get; set; }
+		[Required]
+		public required string LocationBarangay { get; set; }
+		[Required]
+		public required string LocationCity { get; set; }
+		[Required]
+		public required string LocationProvince { get; set; }
+		public string? Description { get; set; }
+	  }
+	}
+	
+	public class ProofOfBusinessDTO 
+	{
+		public class Create 
+		{
+			[Required]
+			public required string OwnerValidID1 { get; set; }
+			[Required]
+			public required string OwnerValidID2 { get; set; }
+			[Required]
+			public required string BusinessPermit { get; set; }
+			[Required]
+			public required string BIRPermit { get; set; }
+		}
+	}
 }
--- Models/Karenderya.cs
+++ Models/Karenderya.cs
@@ -3,49 +3,51 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TomNam.Models
 {
-    public class Karenderya
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
+	public class Karenderya
+	{
+		public Guid Id { get; set; } = Guid.NewGuid();
 
-        [Required]
-        public required string UserId { get; set; }
+		[Required]
+		public required string UserId { get; set; }
 
-        [Required]
-        [ForeignKey("UserId")]
-        public required User User { get; set; }
+		[Required]
+		[ForeignKey("UserId")]
+		public required User User { get; set; }
 
-        [Required]
-        [MaxLength(50)]
-        public required string Name { get; set; }
+		[Required]
+		[MaxLength(50)]
+		public required string Name { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public required string LocationStreet { get; set; }
+		[Required]
+		[MaxLength(100)]
+		public required string LocationStreet { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public required string LocationBarangay { get; set; }
+		[Required]
+		[MaxLength(100)]
+		public required string LocationBarangay { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public required string LocationCity { get; set; }
+		[Required]
+		[MaxLength(100)]
+		public required string LocationCity { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public required string LocationProvince { get; set; }
+		[Required]
+		[MaxLength(100)]
+		public required string LocationProvince { get; set; }
 
-        [Required]
-        public required DateOnly DateFounded { get; set; }
+		[Required]
+		public required DateOnly DateFounded { get; set; }
 
-        [MaxLength(500)]
-        public string? Description { get; set; }
+		[MaxLength(500)]
+		public string? Description { get; set; }
 
-        [MaxLength(2048)]
-        public string? LogoPhoto { get; set; }
+		[MaxLength(2048)]
+		public string? LogoPhoto { get; set; }
 
-        [MaxLength(2048)]
-        public string? CoverPhoto { get; set; }
+		[MaxLength(2048)]
+		public string? CoverPhoto { get; set; }
 
-        public double? Rating { get; set; }
-    }
+		public double? Rating { get; set; }
+		
+		public ProofOfBusiness? proofOfBusiness { get; set; }
+	}
 }
