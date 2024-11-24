46b1e4a82f61789d29bcdeb983bd03be290042f7
Charlene Repuesto <repuestocharlene@gmail.com>
2024-11-24 14:41:31 +0800

    Edited return statements for each functions, and variable types for the ProofOfBusiness Update and Create DTOs.

--- Migrations/20241123163013_Edited karenderya response dto and proofofbusiness response dto.Designer.cs
+++ Migrations/20241123163013_Edited karenderya response dto and proofofbusiness response dto.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TomNam.Data;
@@ -11,9 +12,11 @@ using TomNam.Data;
 namespace TomNam.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241123163013_Edited karenderya response dto and proofofbusiness response dto")]
+    partial class Editedkarenderyaresponsedtoandproofofbusinessresponsedto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -223,9 +226,7 @@ namespace TomNam.Migrations
                         .HasColumnType("character varying(500)");
 
                     b.Property<bool?>("IsVerified")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("boolean")
-                        .HasDefaultValue(false);
+                        .HasColumnType("boolean");
 
                     b.Property<string>("LocationBarangay")
                         .IsRequired()
