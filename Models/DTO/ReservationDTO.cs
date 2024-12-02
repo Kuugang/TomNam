6bfbbd43a2c00c04a16482de3aba5c74750aa8f9
Dio <cyzero69@gmail.com>
2024-12-02 14:45:58 +0800

    initial reservation

---
+++ Models/DTO/ReservationDTO.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TomNam.Models.DTO
+{
+    public class ReservationRequestDTO
+    {
+        public class Create
+        {
+            [Required]
+            public required List<String> CartItemIds { get; set; }
+
+            [Required]
+            public required DateTime  ReserveDateTime { get; set; }
+
+            [Required]
+            public required string ModeOfPayment { get; set; }
+        }
+
+        // mag update if mag cancel
+    }
+
+    public class ReservationResponseDTO
+    {
+        public Guid Id { get; set; }
+        public required Guid CustomerProfileId { get; set; }
+        public required CustomerProfile Customer { get; set; }
+        public required Karenderya Karenderya { get; set; }
+        public required DateTime ReserveDateTime { get; set; }
+        public required double Total { get; set; }
+        public required string ModeOfPayment { get; set; }
+        public required List<ReservedItem> ReservedItems { get; set; }
+
+
+    }
+
+    public class ReservedItem
+    {
+        public Guid Id { get; set; }
+        public Guid FoodId { get; set; }
+        public Food Food { get; set; }
+        public int Quantity { get; set; }
+    }
+}
